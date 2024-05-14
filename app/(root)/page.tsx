d8519a6202cd302eb9418f1f008b924af48005ed
Zakaria-Bli <zakaria.bli99@gmail.com>
2024-05-14 18:39:59 +0100

    Add right sidebar UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import HeaderBox from "@/components/HeaderBox"
+import RightSidebar from "@/components/RightSidebar"
 import TotalBalanceBox from "@/components/TotalBalanceBox"
 import React from "react"
 
 export default function Home() {
-  const loggedIn = { firstName: "Zak" }
+  const loggedIn = {
+    firstName: "Zak",
+    lastName: "Code",
+    email: "test@test.com",
+  }
 
   return (
     <section className="home">
@@ -22,7 +27,14 @@ export default function Home() {
             totalCurrentBalance={1250.35}
           />
         </header>
+        RECENT TRANSACTIONS
       </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 500 }]}
+      />
     </section>
   )
 }
