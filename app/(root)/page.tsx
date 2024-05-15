b930cb32923a042da0f5075d7351332f407449f1
Zakaria-Bli <zakaria.bli99@gmail.com>
2024-05-15 14:24:58 +0100

    Implement sign-in, sign-up, user action

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,11 @@
 import HeaderBox from "@/components/HeaderBox"
 import RightSidebar from "@/components/RightSidebar"
 import TotalBalanceBox from "@/components/TotalBalanceBox"
+import { getLoggedInUser } from "@/lib/actions/user.actions"
 import React from "react"
 
-export default function Home() {
-  const loggedIn = {
-    firstName: "Zak",
-    lastName: "Code",
-    email: "test@test.com",
-  }
+export default async function Home() {
+  const loggedIn = await getLoggedInUser()
 
   return (
     <section className="home">
@@ -17,7 +14,7 @@ export default function Home() {
           <HeaderBox
             type="greeting"
             title="Welcome"
-            user={loggedIn?.firstName || "Guest"}
+            user={loggedIn?.name || "Guest"}
             subtext="Access and manage your account and transactions efficiently."
           />
 
--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -22,7 +22,7 @@ import CustomInput from "./CustomInput"
 import { authFormSchema } from "@/lib/utils"
 import { Loader2 } from "lucide-react"
 import { useRouter } from "next/navigation"
-// import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions';
+import { signIn, signUp } from "@/lib/actions/user.actions"
 // import PlaidLink from './PlaidLink';
 
 const AuthForm = ({ type }: { type: string }) => {
@@ -49,30 +49,31 @@ const AuthForm = ({ type }: { type: string }) => {
       // Sign up with Appwrite & create plaid token
 
       if (type === "sign-up") {
-        const userData = {
-          firstName: data.firstName!,
-          lastName: data.lastName!,
-          address1: data.address1!,
-          city: data.city!,
-          state: data.state!,
-          postalCode: data.postalCode!,
-          dateOfBirth: data.dateOfBirth!,
-          ssn: data.ssn!,
-          email: data.email,
-          password: data.password,
-        }
+        // const userData = {
+        //   firstName: data.firstName!,
+        //   lastName: data.lastName!,
+        //   address1: data.address1!,
+        //   city: data.city!,
+        //   state: data.state!,
+        //   postalCode: data.postalCode!,
+        //   dateOfBirth: data.dateOfBirth!,
+        //   ssn: data.ssn!,
+        //   email: data.email,
+        //   password: data.password,
+        // }
 
-        // const newUser = await signUp(userData);
+        const newUser = await signUp(data)
 
-        // setUser(newUser);
+        setUser(newUser)
       }
 
       if (type === "sign-in") {
-        // const response = await signIn({
-        //   email: data.email,
-        //   password: data.password,
-        // })
-        // if(response) router.push('/')
+        const response = await signIn({
+          email: data.email,
+          password: data.password,
+        })
+
+        if (response) router.push("/")
       }
     } catch (error) {
       console.log(error)
